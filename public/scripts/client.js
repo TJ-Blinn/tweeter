35233dda2d39bc9139037552b8f7d0f5d54f65f1
TJ-Blinn <tjblinn@hotmail.com>
2021-09-15 04:23:42 +0000

    added createTweetElements, renderTweets functions, with  .empty

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,99 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+// tweet data object: user: {"name": "Newton", "avatars": "https://url.com", "handle": "@handle"}
+$(document).ready(function() {
+  
+  const createTweetElement = (tweetData) => {
+    let $tweet = $(`
+  <article>
+          <header>
+          <img src="${tweetData.user.avatars}">
+            <h3>${tweetData.user.name}</h3><span class="handle">${tweetData.user.handle}</span>
+          </header>
+          <main>
+            <p>${tweetData.content.text}</p>
+          </main>
+
+          <footer>
+            <div>
+              <span class="timestamp">${tweetData.created_at}</span>
+              <div class="footer_icons">
+                <i class="fas fa-flag"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="fas fa-heart"></i>
+              </div>
+            </div>
+
+          </footer>
+        </article>
+  `);
+    return $tweet;
+  };
+
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+  // Fake data taken from initial-tweets.json for array of tweets
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+    const $container = $('.tweets-container');
+    $container.empty(); // tweets container is emptying out previous hard-coded tweets
+
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet); // tweet here is an individual tweet from the data array
+      $container.prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  
+
+  };
+  renderTweets(data);
+
+});
